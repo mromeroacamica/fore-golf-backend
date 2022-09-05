56e794eb20bccd651225c72c5a13509c6f7a6262
Sistemas <sistemas@boxcustodia.com>
2022-09-05 12:24:27 -0300

    proyectos casi terminado

--- controller/proyectoController.js
+++ controller/proyectoController.js
@@ -34,8 +34,51 @@ const obtenerProyecto = async (req, res) => {
   }
 };
 
-const editarProyecto = async (req, res) => {};
-const eliminarProyecto = async (req, res) => {};
+const editarProyecto = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const proyecto = await Proyecto.findById(id);
+    if (!proyecto) {
+      const error = new Error("Proyecto no encontrado.");
+      return res.status(404).json({ msg: error.message });
+    }
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+      const error = new Error("Sin permisos de acceso.");
+      return res.status(401).json({ msg: error.message });
+    }
+    proyecto.nombre = req.body.nombre || proyecto.nombre;
+    proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+    proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+    proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+    try {
+      const proyectoAlmacenado = await proyecto.save();
+      res.json(proyectoAlmacenado);
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+const eliminarProyecto = async (req, res) => {
+  const { id } = req.params;
+  const proyecto = await Proyecto.findById(id);
+  if (!proyecto) {
+    const error = new Error("Proyecto no encontrado.");
+    return res.status(404).json({ msg: error.message });
+  }
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Sin permisos de acceso.");
+    return res.status(401).json({ msg: error.message });
+  }
+  try {
+    await proyecto.deleteOne();
+    res.status(200).json({ msg: "Proyecto eliminado" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 const agregarColaboradores = async (req, res) => {};
 const eliminarColaborador = async (req, res) => {};
 const obtenerTareas = async (req, res) => {};
