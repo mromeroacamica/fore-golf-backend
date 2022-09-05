307585ca9b63207619b3ffab48e35390c29ba608
Sistemas <sistemas@boxcustodia.com>
2022-09-05 12:55:40 -0300

    crear tareas finalizado

---
+++ controller/tareaController.js
@@ -0,0 +1,40 @@
+import Proyecto from "../models/Proyectos.js";
+import Tarea from "../models/Tarea.js";
+
+const agregarTarea = async (req, res) => {
+  const { proyecto } = req.body;
+  const existeProyecto = await Proyecto.findById(proyecto);
+  if (!existeProyecto) {
+    const error = new Error("El proyecto no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (existeProyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes permisos de acceso.");
+    return res.status(404).json({ msg: error.message });
+  }
+  try {
+    const tareaAlmacenada = await Tarea.create(req.body);
+    res.json(tareaAlmacenada);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const obtenerTarea = async (req, res) => {
+  const { id } = req.params;
+};
+
+const actualizarTarea = async (req, res) => {};
+
+const eliminarTarea = async (req, res) => {};
+
+const cambiarEstado = async (req, res) => {};
+
+export {
+  agregarTarea,
+  obtenerTarea,
+  actualizarTarea,
+  eliminarTarea,
+  cambiarEstado,
+};
