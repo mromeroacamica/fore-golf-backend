13939ad50731bf4be61f8a17603b5ec95d571c5b
Sistemas <sistemas@boxcustodia.com>
2022-09-13 16:50:19 -0300

    validacion de posibles combinaciones de configuraciones

--- controller/teeSalidaController.js
+++ controller/teeSalidaController.js
@@ -78,13 +78,42 @@ const obtenerConfigBooking = async (req, res) => {
 };
 
 const nuevoConfigBooking = async (req, res) => {
+  const { dayOfWeek, club, prioridadSocio, prioridadSocioHastaDia } = req.body;
   const configBooking = new ConfigBooking(req.body);
+  const { usuario } = req;
+  console.log(usuario);
   try {
     //Validar que no exista una config para ese dia ya creada en ese club && validar si todos los dias tienen una config
-
+    const configBookingClub = await ConfigBooking.find({
+      $and: [{ dayOfWeek: { $eq: dayOfWeek } }, { club: { $eq: club } }],
+    });
+    if (configBookingClub.length > 0 || !configBookingClub) {
+      const error2 = new Error(
+        `Ya tienes una Config de reserva para el día ${dayOfWeek}.`,
+      );
+      return res.status(404).json({ msg: error2.message });
+    }
     //Validar que tengas permisos para crear esta config
+    if (!usuario.clubUser || usuario.club !== club) {
+      const error2 = new Error(`No tienes permisos para esta acción.`);
+      return res.status(404).json({ msg: error2.message });
+    }
+    //Validar que el dia este entre 0 y 6
+    if (dayOfWeek < 0 || dayOfWeek > 6) {
+      const error2 = new Error(
+        `Error al agregar Config a un día que no existe.`,
+      );
+      return res.status(404).json({ msg: error2.message });
+    }
+    // Validar que no ponga prioridadSocioHastaDia si esta prioridad en false
+    if (!prioridadSocio && prioridadSocioHastaDia) {
+      const error2 = new Error(
+        `Error al agregar Config con condiciones no compatibles.`,
+      );
+      return res.status(404).json({ msg: error2.message });
+    }
     const configBookingAlmacenado = await configBooking.save();
-    res.json(configBookingAlmacenado);
+    res.status(201).json(configBookingAlmacenado);
   } catch (error) {
     console.log(error);
     const error2 = new Error("Config de reserva no se pudo agregar.");
